55863035412f1ac15ecac107931e7ab19cae8c3c
Goncalo Palaio <goncalo.oliveira@wit-software.com>
2019-10-01 23:03:26 +0100

    working example with the simpler stb_freetype api

--- main.cpp
+++ main.cpp
@@ -13,11 +13,13 @@
 #include "stb_rect_pack.h"
 #include "stb_truetype.h"
 
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
 
 #define WINDOW_W 600
 #define WINDOW_H 600
@@ -126,6 +128,32 @@ inline float *push_v5_arr(float *v, float x, float y, float z, float s, float t)
     return v + 5;
 }
 
+float* push_textured_quad_arr(float*v, float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1) {
+    // quad 1 2 3 4
+    // -->
+    // triangle 1 2 3
+    // triangle 3 4 1
+
+    //v = push_v5_arr(v, x0, y0, 0, s0, t0); // 1
+    //v = push_v5_arr(v, x1, y0, 0, s1, t0); // 2
+    //v = push_v5_arr(v, x1, y1, 0, s1, t1); // 3
+    //v = push_v5_arr(v, x0, y1, 0, s0, t1); // 4
+
+    v = push_v5_arr(v, x0, y0, 1.0, s0, t0); // 1
+    v = push_v5_arr(v, x1, y0, 1.0, s1, t0); // 2
+    v = push_v5_arr(v, x1, y1, 1.0, s1, t1); // 3
+
+    v = push_v5_arr(v, x1, y1, 1.0, s1, t1); // 3
+    v = push_v5_arr(v, x0, y1, 1.0, s0, t1); // 4
+    v = push_v5_arr(v, x0, y0, 1.0, s0, t0); // 1
+
+    return v;
+}
+
+float* push_textured_quad_scaled_arr(float*v, float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1, float scale_x, float scale_y) {
+    return push_textured_quad_arr(v, x0 * scale_x, y0 * scale_y, x1 * scale_x, y1 * scale_y, s0, t0, s1, t1);
+}
+
 void set_float3(float3 *v, float x, float y, float z) {
     v->x = x;
     v->y = y;
@@ -198,6 +226,28 @@ int compile_shader_program(const char* str_vert_shader, const char* str_frag_sha
 }
 
 
+GLuint upload_new_texture(int width, int height, int channels, unsigned char* pixels) {
+    GLuint tex;
+
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    if (channels == 3) {
+       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    } else if(channels == 4) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    } else {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, pixels);
+    }
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return tex;
+}
+
 int main(int argc, char const *argv[]) {
 	printf("Hello!\n");
 
@@ -241,8 +291,11 @@ int main(int argc, char const *argv[]) {
         "varying vec2 vuvs;"
 		"uniform sampler2D texture;"
         "void main() {"
-        "gl_FragColor = texture2D(texture, vuvs);"
-        //"gl_FragColor = vec4(0.1,0.1,0.1, 0.1) + texture2D(texture, vuvs);"
+        "vec4 c = texture2D(texture, vuvs);"
+        "if (c.r < 0.2) {"
+        "   c = vec4(0.0);"
+        "}"
+        "gl_FragColor = c;"
         "}";
 	printf("Compiling shader\n");
     GLuint main_shader = compile_shader_program(vs_source,
@@ -260,9 +313,9 @@ int main(int argc, char const *argv[]) {
 	float model_matrix[] = M_MAT4_IDENTITY();
 
     float aspect = WINDOW_W / (float)WINDOW_H;
-    m_mat4_perspective(projection_matrix, 10.0, aspect, 0.1, 100.0);
+    m_mat4_perspective(projection_matrix, 10.0, aspect, 0.1, 999.0);
 
-	set_float3(&camera_position, 0, 1, 5);
+	set_float3(&camera_position, 0, 1, 100);
 	set_float3(&camera_direction, 0 - camera_position.x ,0 - camera_position.y , 0 - camera_position.z);
 	set_float3(&camera_up, 0,1,0);
 	
@@ -274,63 +327,76 @@ int main(int argc, char const *argv[]) {
 
 	float* buf = vertex_data;
     {
-		float scale = 1.0;
-		buf = push_v5_arr(buf, -scale, scale, 0.0, 0.0, 1.0);
-		buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
-		buf = push_v5_arr(buf, scale, scale, 0.0, 1.0, 1.0);
-		buf = push_v5_arr(buf, -scale, scale, 0.0, 0.0, 1.0);
-		buf = push_v5_arr(buf, -scale, -scale, 0.0, 0.0, 0.0);
-		buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
+		
+        buf = push_textured_quad_arr(buf, -10, -10, 10, 10, 0, 0, 1, 1);
+		// float scale = 1.0;
+        //buf = push_v5_arr(buf, -scale, scale, 0.0, 0.0, 1.0);
+		//buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
+		//buf = push_v5_arr(buf, scale, scale, 0.0, 1.0, 1.0);
+		//buf = push_v5_arr(buf, -scale, scale, 0.0, 0.0, 1.0);
+		//buf = push_v5_arr(buf, -scale, -scale, 0.0, 0.0, 0.0);
+		//buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
     }
 
     printf("Loading font !!\n");
     const unsigned char* font_file = read_entire_file("Roboto.ttf");
     
-    stbtt_fontinfo info;
-    if (!stbtt_InitFont(&info, font_file, 0)) {
-        printf("init font failed\n");
-        return 1;
-    } else {
-        printf("font loaded\n");
-    }
+    int font_size = 200;
 
-    int font_size = 190;
-
-    //const char font_first_char = 'A';
-    //const int font_char_count = 6;
-    const char font_first_char = 'A';
-    const int font_char_count = 10;
+    const char font_first_char = ' ';
+    const int font_char_count = '~' - ' ';
     
-    const int bitmap_width = 512;
-    const int bitmap_height = 256;
+    const int bitmap_width = 1024;
+    const int bitmap_height = 1024;
     const int oversample_x = 1;
     const int oversample_y = 1;
 
     printf("font_init - font_first_char: %d\n", font_first_char);
     printf("font_init - font_char_count: %d\n", font_char_count);
+    uint8_t* bitmap = (uint8_t *) malloc(sizeof(uint8_t) * bitmap_width * bitmap_height);
+    stbtt_bakedchar cdata[96]; // ASCII 32..126 is 95 glyphs
 
-    uint8_t* bitmap = (uint8_t *) malloc(
-            sizeof(uint8_t) * bitmap_width * bitmap_height);
-    stbtt_packedchar* font_char_data = (stbtt_packedchar *) malloc(
-            sizeof(stbtt_packedchar) * font_char_count);
+// This implementation of the following api is not working?:
+//           stbtt_PackBegin()
+//           stbtt_PackSetOversampling()          -- for improved quality on small fonts
+//           stbtt_PackFontRanges()               -- pack and renders
+//           stbtt_PackEnd()
+//           stbtt_GetPackedQuad()
+//
+//
+//    stbtt_fontinfo info;
+//    if (!stbtt_InitFont(&info, font_file, 0)) {
+//        printf("init font failed\n");
+//        return 1;
+//    } else {
+//        printf("font loaded\n");
+//    }
+//    
+//    uint8_t* bitmap = (uint8_t *) malloc(
+//            sizeof(uint8_t) * bitmap_width * bitmap_height);
+//    stbtt_packedchar* font_char_data = (stbtt_packedchar *) malloc(
+//            sizeof(stbtt_packedchar) * font_char_count);
+//
+//  printf("font_init - pack_begin\n");
+//    stbtt_pack_context context;
+//    if (!stbtt_PackBegin(&context, bitmap, bitmap_width, bitmap_height, 0, 1,
+//                         NULL)) {
+//        printf("failed to pack begin font\n");
+//        assert(0);
+//    }
+//
+//    printf("font_init - pack_font_range\n");
+//    stbtt_PackSetOversampling(&context, oversample_x, oversample_y);
+//    if (!stbtt_PackFontRange(&context, font_file, 0, font_size, font_first_char,
+//                             font_char_count, font_char_data)) {
+//        printf("failed to pack font\n");
+//        assert(0);
+//    }
+//    stbtt_PackEnd(&context);
+//    printf("font_init - pack_end\n");
 
-	printf("font_init - pack_begin\n");
-    stbtt_pack_context context;
-    if (!stbtt_PackBegin(&context, bitmap, bitmap_width, bitmap_height, 0, 1,
-                         NULL)) {
-        printf("failed to pack begin font\n");
-        assert(0);
-    }
-
-    printf("font_init - pack_font_range\n");
-    stbtt_PackSetOversampling(&context, oversample_x, oversample_y);
-    if (!stbtt_PackFontRange(&context, font_file, 0, font_size, font_first_char,
-                             font_char_count, font_char_data)) {
-        printf("failed to pack font\n");
-        assert(0);
-    }
-    stbtt_PackEnd(&context);
-    printf("font_init - pack_end\n");
+    // Using simpler API with stbtt_BakeFontBitmap
+    stbtt_BakeFontBitmap(font_file,0, font_size, bitmap, bitmap_width, bitmap_height, font_first_char, font_char_count, cdata); // no guarantee this fits!
 	
 	glEnable(GL_TEXTURE_2D);	
 	printf("font_init - uploading texture\n");
@@ -339,47 +405,49 @@ int main(int argc, char const *argv[]) {
 
     stbi_write_png("font.png", bitmap_width, bitmap_height, 1, bitmap, 0);
 
-    glGenTextures(1, &font_texture);
-    glBindTexture(GL_TEXTURE_2D, font_texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, bitmap_width, bitmap_height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-	                     bitmap);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    font_texture = upload_new_texture(bitmap_width, bitmap_height, 1, bitmap);
     
     delete bitmap;
     delete font_file;
 
-   	int character_vertex_data_size = sizeof(float) * 6 * 5;
+    int max_characters = 10;
+    int elements_per_vertex = 5;
+    int vertices_per_quad = 6;
+   	int character_vertex_data_size = sizeof(float) * vertices_per_quad * elements_per_vertex * max_characters;
 	float* character_vertex_data = (float *) malloc(character_vertex_data_size);
-    {
-    	float x = 0;
-    	float y = 0;
-		stbtt_aligned_quad q;
+    
+    float x = 0;
+    float y = -110;
 
-    	char character = 'B';
+    const char* text = "Hello!";
+    const int text_len = strlen(text);
+    
+    buf = character_vertex_data;
+    stbtt_aligned_quad q;
+    for (int i = 0; i < text_len; ++i) {
+    	char character = text[i];
+    	int index_to_char = character - font_first_char;
+    	
+// This is not working?
+//      stbtt_aligned_quad q;
+//		stbtt_GetPackedQuad(font_char_data,
+//                                bitmap_width,
+//                                bitmap_height,
+//                                index_to_char,
+//                                &x,
+//                                &y,
+//                                &q,
+//                                1);
 
-		stbtt_GetPackedQuad(font_char_data,
-                                bitmap_width,
-                                bitmap_height,
-                                character - font_first_char,
-                                &x,
-                                &y,
-                                &q,
-                                1);
+        
+        stbtt_GetBakedQuad(cdata, bitmap_width, bitmap_height, index_to_char, &x,&y, &q, 1);//1=opengl & d3d10+,0=d3d9
 
-
-		float* buf = character_vertex_data;
 	    {
-	    	float xf = 0.01;
-	    	float yf = 0.01;
-	    	printf("rendering character to position\n");
-			float scale = 1.0;
-			buf = push_v5_arr(buf, xf * q.x0 + 1.0f, yf * q.y0, 2.0, q.s0, q.t0 - 0.2);
-			buf = push_v5_arr(buf, xf * q.x1 + 1.0f, yf * q.y0, 2.0, q.s1, q.t0 - 0.2);
-			buf = push_v5_arr(buf, xf * q.x1 + 1.0f, yf * q.y1, 2.0, q.s1, q.t1 - 0.2);
-			buf = push_v5_arr(buf, xf * q.x0 + 1.0f, yf * q.y0, 2.0, q.s0, q.t0 - 0.2);
-			buf = push_v5_arr(buf, xf * q.x1 + 1.0f, yf * q.y1, 2.0, q.s1, q.t1 - 0.2);
-			buf = push_v5_arr(buf, xf * q.x0 + 1.0f, yf * q.y1, 2.0, q.s0, q.t1 - 0.2);
+            // invert the y coordinates since the texture is up side down.
+            q.y0 = -q.y0;
+            q.y1 = -q.y1;
+            printf("x: %f y: %f\n", x, y);
+            buf = push_textured_quad_scaled_arr(buf, q.x0, q.y0, q.x1, q.y1, q.s0, q.t0, q.s1, q.t1, 0.1, 0.1);
 	    }
     }
 
@@ -391,19 +459,8 @@ int main(int argc, char const *argv[]) {
 	    unsigned char *pixels = stbi_load("texture_map.png", &width, &height, &channels, 0);
 	    assert(pixels != NULL);
 
-	    glGenTextures(1, &test_texture);
-	    glBindTexture(GL_TEXTURE_2D, test_texture);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        test_texture = upload_new_texture(width, height, channels, pixels);
 
-	    if (channels == 3) {
-	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-	    } else {
-	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-	                     pixels);
-	    }
-
-	    glBindTexture(GL_TEXTURE_2D, 0);
 	    stbi_image_free(pixels);
 
 	    printf("Loaded test texture: %d %d %d\n", width, height, channels);
@@ -417,21 +474,8 @@ int main(int argc, char const *argv[]) {
 	    unsigned char *pixels = stbi_load("font.png", &width, &height, &channels, 0);
 	    assert(pixels != NULL);
 
-	    glGenTextures(1, &font_texture_from_file);
-	    glBindTexture(GL_TEXTURE_2D, font_texture_from_file);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	    if (channels == 3) {
-	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-	    } else if(channels == 4) {
-	        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-	                     pixels);
-	    } else {
-	    	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE,
-	                     pixels);
-	    }
+        font_texture_from_file = upload_new_texture(width, height, channels, pixels);
 
-	    glBindTexture(GL_TEXTURE_2D, 0);
 	    stbi_image_free(pixels);
 
 	    printf("Loaded font texture from file: %d %d %d\n", width, height, channels);
@@ -440,6 +484,8 @@ int main(int argc, char const *argv[]) {
     printf("Entering Render Loop\n");
     GL_ERR;
 	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     while(!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -488,7 +534,7 @@ int main(int argc, char const *argv[]) {
 			// @NOTE Texture loading from the ttf file gives a flipped texture which will certainly lead to incorrect results. Let's debug from a file written to disk for now.
 
 			//glBindTexture(GL_TEXTURE_2D, font_texture);
-			glBindTexture(GL_TEXTURE_2D, font_texture_from_file);
+			glBindTexture(GL_TEXTURE_2D, font_texture);
 			//glBindTexture(GL_TEXTURE_2D, test_texture);
 
 			int bytes_per_float = sizeof(float);
@@ -499,7 +545,7 @@ int main(int argc, char const *argv[]) {
 			glEnableVertexAttribArray(uvs);
 			glVertexAttribPointer(uvs, 2, GL_FLOAT, GL_FALSE, stride, character_vertex_data + 3);
 
-			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glDrawArrays(GL_TRIANGLES, 0, vertices_per_quad * text_len);
         }
         glUseProgram(0);
 
